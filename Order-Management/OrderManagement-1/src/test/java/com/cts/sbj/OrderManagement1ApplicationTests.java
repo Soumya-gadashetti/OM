6cd4adc2b99c3a8318bb8b1d4b9570738e8b0c08
Soumya-gadashetti <60088012+Soumya-gadashetti@users.noreply.github.com>
2020-04-28 00:10:18 -0400

    Order-Management

---
+++ Order-Management/OrderManagement-1/src/test/java/com/cts/sbj/OrderManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.sbj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagement1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
